c99824b0221b2851c3e460a85253853a297edcc3
Christopher Capobianco <ccapo@users.noreply.github.com>
2024-10-16 11:25:58 -0400

    Add project details for movie recommendation project

--- projects/02_Movie_Recommendation.py
+++ projects/02_Movie_Recommendation.py
@@ -31,6 +31,16 @@ def get_recommendations(title, cosine_sim = cosine_sim):
     return movies
 
 # Get movie recommendations
+st.markdown("#### What is a Recommendation System?")
+st.markdown("Recommendation systems suggest recommendations to users depending on a variety of criteria.")
+st.markdown('''There are 3 types of recommendation systems:
+1. Demographic Filtering: The recommendations are the same for every user. They are generalized, not personalized. These types of systems are behind sections like *Top Trending*.
+2. Content-based Filtering: These suggest recommendations based on the item metadata (movie, product, song, etc). Here, the main idea is if a user likes an item, then the user will also like items similar to it.
+3. Collaboration-based Filtering: These systems make recommendations by grouping the users with similar interests. For this system, metadata of the item is not required.
+''')
+st.markdown("In this project, we are building a **Content-based** recommendation engine for movies titles.")
+st.markdown("To build this recommendation system, we will be using almost 5k movies and credits from the TMDB dataset available at Kaggle")
+
 st.markdown("Enter the title of a movie to see a list of 10 recommendations (e.g. The Dark Knight Rises, The Avengers, etc)")
 title = st.text_input("Movie Title")
 if title != "":
